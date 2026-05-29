e67984d50465c9ac1d2bb6fc65c90001dd2421db
EugeneLee0154 <eujunelee2005@gmail.com>
2026-05-30 04:59:13 +1000

    Updated choropleth map

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,11 +1,21 @@
+var choroplethMap = "visitor_expenditure_choropleth.vg.json";
 var flowMap = "visitor_flow_map.vg.json";
 
-vegaEmbed("#flow_map", flowMap, {
-  actions: false,
-  renderer: "svg"
-}).then(function(result) {
-  // Access the Vega view instance as result.view.
-}).catch(function(error) {
-  document.querySelector("#flow_map").innerHTML = '<pre class="error">' + error + '</pre>';
-  console.error(error);
+function embedMap(container, spec, options) {
+  var embedOptions = Object.assign({
+    actions: false,
+    renderer: "svg"
+  }, options);
+
+  vegaEmbed(container, spec, embedOptions).then(function(result) {
+    // Access the Vega view instance as result.view.
+  }).catch(function(error) {
+    document.querySelector(container).innerHTML = '<pre class="error">' + error + '</pre>';
+    console.error(error);
+  });
+}
+
+embedMap("#choropleth_map", choroplethMap, {
+  bind: "#choropleth_controls"
 });
+embedMap("#flow_map", flowMap);
